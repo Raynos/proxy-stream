d15829ba59152a9b25814a808ae5ff7e9daaeef2
Raynos <raynos2@gmail.com>
2012-09-27 21:43:09 -0700

    end buffering

--- examples/simple.js
+++ examples/simple.js
@@ -23,6 +23,11 @@ var mapped = map(from([1,2,3]), function (chunk) {
     , doubleMapped = map(mapped, function (chunk) {
         return chunk * 3
     })
+    , asyncMapped = mapAsync(from([1,2,3]), function (chunk, callback) {
+        setTimeout(function () {
+            callback(null, chunk * 4)
+        }, 1000)
+    })
 
 mapped.pipe(to([], function (list) {
     console.log("mapped", list)
@@ -32,10 +37,25 @@ doubleMapped.pipe(to([], function (list) {
     console.log("double mapped", list)
 }))
 
+asyncMapped.pipe(to([], function (list) {
+    console.log("async mapped", list)
+}))
+
 function map(stream, iterator) {
     return proxy(stream, transformation)
 
     function transformation(chunk, next) {
         next(iterator(chunk))
+    }
+}
+
+function mapAsync(stream, iterator) {
+    return proxy(stream, transformation)
+
+    function transformation(chunk, next, end) {
+        iterator(chunk, function (err, value) {
+            next(value)
+            end()
+        })
     }
 }
--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var ReadWriteStream = require("read-write-stream")
 
 module.exports = proxy
 
-function proxy(stream, transformation, readEnd, writeEnd) {
-    writeEnd = writeEnd || defaultWriteEnd
-    readEnd = readEnd || defaultReadEnd
-
+function proxy(stream, transformation) {
     var proxied = ReadWriteStream(write, writeEnd, read).stream
+        , writeEndCount = 0
+        , readEndCount = 0
+        , readEnded = false
+        , writeEnded = false
         , pipeStream
 
     proxied.pipe = handlePipe
@@ -18,24 +19,13 @@ function proxy(stream, transformation, readEnd, writeEnd) {
 
     return proxied
 
-    function handlePipe(dest) {
-        if (!pipeStream) {
-            pipeStream = ReadWriteStream(writePipe).stream
-            stream.pipe(pipeStream)
-        }
-        return pipeStream.pipe(dest)
-    }
-
     function write(chunk, queue) {
-        transformation(chunk, stream.write)
-    }
-
-    function defaultWriteEnd() {
-        stream.end()
-    }
-
-    function defaultReadEnd() {
-        stream.emit("end")
+        if (transformation.length === 3) {
+            writeEndCount++
+            return transformation(chunk, stream.write, decrementWriteCount)
+        } else {
+            return transformation(chunk, stream.write)
+        }
     }
 
     function read(bytes, queue) {
@@ -44,14 +34,79 @@ function proxy(stream, transformation, readEnd, writeEnd) {
             return null
         }
 
-        transformation(chunk, queue.push)
+        if (transformation.length === 3) {
+            readEndCount++
+            transformation(chunk, queue.push, decrementReadCount)
+        } else {
+            transformation(chunk, queue.push)
+        }
+
         if (queue.length === 0) {
             return read(bytes, queue)
         }
         return queue.shift()
     }
 
-    function writePipe(chunk, queue) {
-        transformation(chunk, queue.push)
+    function decrementWriteCount() {
+        writeEndCount--
+        if (writeEnded && writeEndCount === 0) {
+            stream.end()
+        }
+    }
+
+    function decrementReadCount() {
+        readEndCount--
+        if (readEnded && readEndCount === 0) {
+            proxied.emit("end")
+        }
+    }
+
+    function writeEnd() {
+        writeEnded = true
+        if (writeEndCount === 0) {
+            stream.end()
+        }
+    }
+
+    function readEnd() {
+        readEnded = true
+        if (readEndCount === 0) {
+            proxied.emit("end")
+        }
+    }
+
+    function handlePipe(dest) {
+        var endCount = 0
+            , ended = false
+
+        if (!pipeStream) {
+            pipeStream = ReadWriteStream(pipeWrite, pipeEnd).stream
+            stream.pipe(pipeStream)
+        }
+
+        return pipeStream.pipe(dest)
+
+        function pipeWrite(chunk, queue) {
+            if (transformation.length === 3) {
+                endCount++
+                return transformation(chunk, queue.push, decrementCount)
+            } else {
+                return transformation(chunk, queue.push)
+            }
+        }
+
+        function decrementCount() {
+            endCount--
+            if (ended && endCount === 0) {
+                pipeStream.emit("end")
+            }
+        }
+
+        function pipeEnd() {
+            ended = true
+            if (endCount === 0) {
+                pipeStream.emit("end")
+            }
+        }
     }
 }
